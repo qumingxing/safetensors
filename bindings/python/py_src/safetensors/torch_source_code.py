98a32648c38e974c5784c26c3f8467916f41eaa4
qumingxing <qmx_000@163.com>
2025-02-11 16:41:59 +0800

    [PBI-0.5.2] supports gpu

--- bindings/python/py_src/safetensors/torch_source_code.py
+++ bindings/python/py_src/safetensors/torch_source_code.py
@@ -346,8 +346,10 @@ def load_file(filename: Union[str, os.PathLike], device: Union[str, int] = "cpu"
             #f.seek(offset)
             #buffer = f.read(num_bytes)
             buffer = decrypted_data[offset : offset + num_bytes]
-
-            tensor = torch.frombuffer(buffer, dtype=dtype)
+            if device != "cpu":
+                tensor = torch.frombuffer(buffer, dtype=dtype).to(device)
+            else:
+                tensor = torch.frombuffer(buffer, dtype=dtype)
             if shape:
                 tensor = tensor.reshape(shape)
             result[key] = tensor
